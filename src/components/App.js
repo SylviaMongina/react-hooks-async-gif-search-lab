888871c042b93360b57b481f217028ddce3cbd27
SylviaMonginaBosire <sylvia.bosire@student.moringaschool.com>
2023-01-24 09:56:17 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 
 import NavBar from "./NavBar";
+import GifListContainer from './GifListContainer'
 
 // the App component should render out the GifListContainer component
 
 function App() {
   return (
-    <div>
+    <div >
       <NavBar color="black" title="Giphy Search" />
+      <GifListContainer/>
     </div>
   );
 }
 
 export default App;
+
